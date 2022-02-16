3a35bfd5afb5fee2ff83094d731508aed585fe5c
Gobara1812 <j1812@matsue-ct.jp>
2022-02-16 10:28:57 +0900

    kadai3

---
+++ kadai3.rb
@@ -0,0 +1,13 @@
+for i in 1..100 do
+  if i.modulo(3)==0&&i.modulo(5)==0 then
+    print ("FizzBuzz\n")
+  elsif i.modulo(3)==0 then
+    print ("Fizz\n")
+  elsif i.modulo(5)==0 then
+    print ("Buzz\n")
+  elsif i.modulo(7)==0 then
+    print ("git\n")
+  else
+    print ("i\n")
+  end
+end
